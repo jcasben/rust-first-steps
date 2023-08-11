9bba896eb01875a1bd6ff23d343ebf34ef99a690
jcasben <jcasben02@gmail.com>
2023-08-11 21:20:46 +0200

    [UPDATE] Solved an error in the conditions of the if.

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -10,9 +10,9 @@ fn main() {
     let number = 6;
     if number % 4 == 0 {
         println!("divisible by 4");
-    } else if number % 3{
+    } else if number % 3 == 0{
         println!("divisible by 3");
-    } else if number % 2 {
+    } else if number % 2 == 0{
         println!("divisible by 2");
     } else {
         println!("not divisible by 4, 3 or 2");
