a2192e973506ed3b40c40ff28eac8a2e019f7a0e
jcasben <jcasben02@gmail.com>
2024-02-20 12:35:16 +0100

    [UPDATE] Solved a warning at guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -11,7 +11,7 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
+        if !(1..100).contains(&value) {
             panic!("Guess value must be between 1 and 100, got {}", value);
         }
 
