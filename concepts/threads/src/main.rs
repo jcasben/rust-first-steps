129bd9b123a9ed287f1800b180bbfe64fc6a363d
jcasben <jcasben02@gmail.com>
2024-05-15 09:21:42 +0200

    Update: added mutex

--- concepts/threads/src/main.rs
+++ concepts/threads/src/main.rs
@@ -1,4 +1,4 @@
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 use std::time::Duration;
 
@@ -18,38 +18,57 @@ fn main() {
     // }
 
     // Channels
-    let (tx, rx) = mpsc::channel();
-    let tx1 = tx.clone();
+    // let (tx, rx) = mpsc::channel();
+    // let tx1 = tx.clone();
+    //
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("spawned"),
+    //         String::from("thread")
+    //     ];
+    //
+    //     for v in vals {
+    //         tx.send(v).unwrap();
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    //
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("more"),
+    //         String::from("messages"),
+    //         String::from("from"),
+    //         String::from("other")
+    //     ];
+    //
+    //     for v in vals {
+    //         tx1.send(v).unwrap();
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    //
+    // for received in rx {
+    //     println!("Received: {received}");
+    // }
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("spawned"),
-            String::from("thread")
-        ];
+    // Mutex
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-        for v in vals {
-            tx.send(v).unwrap();
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("from"),
-            String::from("other")
-        ];
-
-        for v in vals {
-            tx1.send(v).unwrap();
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for received in rx {
-        println!("Received: {received}");
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+           let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("counter = {}", counter.lock().unwrap());
 }
