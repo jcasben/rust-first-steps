9e5ab0063aa21eb50c25c55922ca9a3e938dda9e
jcasben <jcasben02@gmail.com>
2024-09-21 01:39:52 +0200

    [UPDATE]: implemented Workers closure

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,35 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter, write};
 use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::Arc;
+use std::sync::Mutex;
+
+#[derive(Debug)]
+pub enum PoolCreationError {
+    InvalidNumberOfThreads
+}
+
+impl Display for PoolCreationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PoolCreationError::InvalidNumberOfThreads => {
+                write!(f, "Cannot create a ThreadPool with no threads")
+            }
+        }
+    }
+}
+
+impl Error for PoolCreationError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
 
 impl ThreadPool {
@@ -12,21 +40,31 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError::InvalidNumberOfThreads);
+        }
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers }
+
+        Ok(ThreadPool { workers, sender })
     }
 
     pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
+        where
+            F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap()
     }
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 
 struct Worker {
     id: usize,
@@ -34,8 +72,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || {
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+                println!("Worker {} got a job! ; Executing...", id);
+                job();
+            }
+        });
         Worker { id, thread }
     }
 }
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,17 +3,17 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration
+    time::Duration,
 };
 use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(4).unwrap_or_else(|err| panic!("{err}"));
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        pool.execute(|| {handle_connection(stream)});
+        pool.execute(|| { handle_connection(stream) });
     }
 }
 
@@ -21,7 +21,7 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename) = match &request_line[..]  {
+    let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
